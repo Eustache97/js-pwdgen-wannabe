c9f1d84565f425a61604c51b6ddab94a2c29c6db
Eustache97 <eubomber97@gmail.com>
2022-10-05 18:20:59 +0200

    aggiungo un po di stile

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ const suffixMail = 21;
 // Chiedere nome dell'utente (Input)
 const userName = prompt("Qual'è il tuo nome?");
 console.log(userName, typeof userName);
+document.getElementById("name").innerHTML = userName ;
 
 // Chiedere cognome dell'utente (Input)
 const userSurname =  prompt("E il tuo cognome?");
 console.log(userSurname, typeof userSurname);
+document.getElementById("surname").innerHTML = userSurname;
 
 // Chiedere il colore preferito dell'utente (Input)
 const userFavColor =  prompt("Qual'è il tuo colore preferito?");
 console.log(userFavColor, typeof userFavColor);
+document.getElementById("favcol").innerHTML = userFavColor;
 
 // Elaborazione
 const passwordGenerated = userName + userSurname + userFavColor + suffixMail;
